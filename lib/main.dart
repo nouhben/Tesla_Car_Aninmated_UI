daa0a74a699fe15c490cc6a3e7f17c9db007894c
nouh ben kadi <benkadi.nouh@icloud.com>
2021-10-06 22:12:51 +0100

    end of ep1

--- lib/main.dart
+++ lib/main.dart
@@ -37,6 +37,10 @@ class HomePage extends StatelessWidget {
         animation: _controller,
         builder: (context, snapshot) {
           return Scaffold(
+            bottomNavigationBar: TeslaBottomNavigationBar(
+              onTap: (index) {},
+              selectedTab: 0,
+            ),
             body: SafeArea(
               child: LayoutBuilder(
                   builder: (context, constraints) => Stack(
@@ -84,5 +88,34 @@ class HomePage extends StatelessWidget {
             ),
           );
         });
+  }
+}
+
+class TeslaBottomNavigationBar extends StatelessWidget {
+  TeslaBottomNavigationBar({
+    Key? key,
+    required this.selectedTab,
+    required this.onTap,
+  }) : super(key: key);
+  final int selectedTab;
+  final ValueChanged<int> onTap;
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      backgroundColor: Colors.black,
+      type: BottomNavigationBarType.fixed,
+      onTap: onTap,
+      currentIndex: selectedTab,
+      items: List.generate(
+        kNavIconsPath.length,
+        (index) => BottomNavigationBarItem(
+          icon: SvgPicture.asset(
+            kNavIconsPath[index],
+            color: index == selectedTab ? primaryColor : Colors.white54,
+          ),
+          label: '',
+        ),
+      ),
+    );
   }
 }
--- lib/shared/constants.dart
+++ lib/shared/constants.dart
@@ -5,3 +5,10 @@ const Color redColor = Color(0xFFFF5368);
 
 const double defaultPadding = 16.0;
 const Duration defaultDuration = Duration(milliseconds: 300);
+
+const List<String> kNavIconsPath = [
+  'assets/icons/Lock.svg',
+  'assets/icons/Charge.svg',
+  'assets/icons/Temp.svg',
+  'assets/icons/Tyre.svg',
+];
